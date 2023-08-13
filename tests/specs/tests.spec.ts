a1e4467b22f60d7e82c63eac081bee20a37c4708
sarialalem1 <sarialalem@gmail.com>
2023-08-13 17:38:45 +0300

    .

--- tests/specs/tests.spec.ts
+++ tests/specs/tests.spec.ts
@@ -1,26 +1,3 @@
-import type {Orchestrator, KafkaOrchestrator} from '../testcontainers/orchestrator.js';
-import {start as startKafka} from '../testcontainers/orchestrator.js';
-import {Producer} from 'kafkajs';
-
 describe('tests', () => {
-    let kafkaOrchestrator: KafkaOrchestrator;
-    let orchestrator: Orchestrator;
-    let producer: Producer;
-
-    beforeAll(async () => {
-        kafkaOrchestrator = await startKafka();
-    }, 18000);
-
-    afterAll(async () => {
-        await kafkaOrchestrator.stop();
-    }, 18000);
-
-    afterEach(async () => {
-        if (producer) {
-            await producer.disconnect();
-        }
-        await orchestrator.stop();
-    });
-
     it('placeholder', async () => {});
 });
