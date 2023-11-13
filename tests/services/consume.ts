75dd5fd333b8188d30a924219cbb3c89858165b4
Guy Segal <guysegal.il@gmail.com>
2023-11-13 13:40:21 +0200

    Revert "."
    This reverts commit 512a44161f836044553128a654b102e748d72357.

--- tests/services/consume.ts
+++ tests/services/consume.ts
@@ -1,19 +1,11 @@
 import {Kafka, KafkaMessage} from 'kafkajs';
 
-export const consume = async (kafka: Kafka, topic: string, parse = true, expectedConsumedCount = 1) => {
-    const consumer = kafka.consumer({groupId: 'orchestrator'});
+export const consume = async (kafka: Kafka, topic: string, parse = true) => {
+    const consumer = kafka.consumer({groupId: 'orchestrator'});    
     await consumer.subscribe({topic: topic, fromBeginning: true});
-    const messages = [];
-    let consumedCount = 0;
     const consumedMessage = await new Promise<KafkaMessage>((resolve) => {
         consumer.run({
-            eachMessage: async ({message}) => {
-                messages.push(message);
-                consumedCount++;
-                if (consumedCount === expectedConsumedCount) {
-                    resolve(message);
-                }
-            },
+            eachMessage: async ({message}) => resolve(message),
         });
     });
     await consumer.disconnect();
