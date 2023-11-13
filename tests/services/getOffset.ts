4bc908e1cb4c415da1755f2e35fb46f2230a6258
Guy Segal <guysegal.il@gmail.com>
2023-11-13 12:01:15 +0200

    aaaaaa

--- tests/services/getOffset.ts
+++ tests/services/getOffset.ts
@@ -3,6 +3,9 @@ import {Kafka} from 'kafkajs';
 export const getOffset = async (kafka: Kafka, groupId: string, topic: string) => {
     const admin = kafka.admin();
     await admin.connect();
+    const x = await admin.listGroups()
+    const y = await admin.listTopics()
+    console.log("1111111111",  x, y)
     const metadata = await admin.fetchOffsets({groupId, topics: [topic]});
     console.log("aaaaaaa", JSON.stringify(metadata))
     admin.disconnect();
